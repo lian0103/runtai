0c8ff853f194b01d30628e1bd05b8a3e2b3ac848
lian0103 <k1319900103@gmail.com>
2020-04-19 19:12:18 +0800

    build

---
+++ build/precache-manifest.d2f1b950658e839e599aaa94e19d3010.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51ae606e40d720b9b5822ab5000078ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "30793e78a1a60fc045e6",
+    "url": "/static/css/main.2533e3d9.chunk.css"
+  },
+  {
+    "revision": "1bd9cfcf05c030590072",
+    "url": "/static/js/2.800a130b.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.800a130b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30793e78a1a60fc045e6",
+    "url": "/static/js/main.2206f01f.chunk.js"
+  },
+  {
+    "revision": "de175500318ff8658fba",
+    "url": "/static/js/runtime-main.cc4081e9.js"
+  },
+  {
+    "revision": "83edd1086c9fe6576f6d4cb58dbed0d6",
+    "url": "/static/media/abs-bg3.83edd108.png"
+  }
+]);
