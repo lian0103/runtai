b8813f4a3738819d497ad51e3e546c8ff2b9a9af
lien0103 <TP2003007@wistronits.com>
2020-07-16 16:23:27 +0800

    Updates

---
+++ precache-manifest.6efb3faa88e14cb4c39d7f2ec0a92314.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a801fdcee0603770bb560cae7889ac53",
+    "url": "/reactWeb/index.html"
+  },
+  {
+    "revision": "009abc6a2a1182b18c07",
+    "url": "/reactWeb/static/css/main.3d822472.chunk.css"
+  },
+  {
+    "revision": "da1f44c43221b96a91be",
+    "url": "/reactWeb/static/js/2.fe2dd02d.chunk.js"
+  },
+  {
+    "revision": "876e403427d7be82fb4e8ac24123aaf3",
+    "url": "/reactWeb/static/js/2.fe2dd02d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "009abc6a2a1182b18c07",
+    "url": "/reactWeb/static/js/main.b403bfb8.chunk.js"
+  },
+  {
+    "revision": "80117c2e5e9126b7bd57",
+    "url": "/reactWeb/static/js/runtime-main.79b48694.js"
+  },
+  {
+    "revision": "3acc4ffa5b3b2f06e10cfb3250abbeba",
+    "url": "/reactWeb/static/media/cat_icon.3acc4ffa.png"
+  },
+  {
+    "revision": "9f9f18b3fca779f48ee83dda8150b245",
+    "url": "/reactWeb/static/media/demo01.9f9f18b3.jpg"
+  },
+  {
+    "revision": "303cdbdbab5d83e14bbc5d1a8dac0a39",
+    "url": "/reactWeb/static/media/demo02.303cdbdb.jpg"
+  },
+  {
+    "revision": "24735fc7d277e1dc54d105a411d18ce0",
+    "url": "/reactWeb/static/media/demo03.24735fc7.jpg"
+  },
+  {
+    "revision": "1bdff69e8a642d08a1bc0e0ca4eb2356",
+    "url": "/reactWeb/static/media/demo04.1bdff69e.jpg"
+  }
+]);
